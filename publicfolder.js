f28bf161fd89692c4e0387c94657ca45afe7ff51
Dave Winer <dave.winer@gmail.com>
2022-09-21 18:22:46 -0400

    v0.5.8

--- publicfolder.js
+++ publicfolder.js
@@ -1,7 +1,7 @@
 var myProductName = "publicFolder", myVersion = "0.5.8";     
 
 /*  The MIT License (MIT)
-	Copyright (c) 2014-2020 Dave Winer
+	Copyright (c) 2014-2022 Dave Winer
 	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
